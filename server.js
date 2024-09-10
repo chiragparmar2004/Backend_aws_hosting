555eccf7a2cefeb1f872b42ec88bcb336308dc19
chiragparmar2004 <chiragparmar4780@gmail.com>
2024-09-10 23:43:44 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello There this is backend of chirag project</h1>");
+});
+
+app.get("/api/v1/checkingServer", (req, res) => {
+  res.send("Successfully Connected to Backend Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
